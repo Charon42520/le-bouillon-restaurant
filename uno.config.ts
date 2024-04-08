f5b9ed191edaa743b5ba619b02e8c88d9da6ffd9
Nicolas Rio <nicolas.rio42@gmail.com>
2024-04-08 14:20:19 +0200

    Exploring

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,21 @@
-import { defineConfig, presetUno, presetAttributify } from "unocss";
+import {
+  defineConfig,
+  presetUno,
+  presetAttributify,
+  presetWebFonts,
+  presetIcons,
+} from "unocss";
 
 export default defineConfig({
-  presets: [presetAttributify(), presetUno()],
+  presets: [
+    presetAttributify(),
+    presetUno(),
+    presetIcons(),
+    presetWebFonts({ provider: "bunny", fonts: { sans: "Hind" } }),
+  ],
+  theme: {
+    colors: {
+      primary: "#ea4900",
+    },
+  },
 });
