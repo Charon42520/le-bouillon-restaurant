445a1774642045435a68661ccf2ed3524bff4d88
Nicolas Rio <nicolas.rio42@gmail.com>
2024-04-29 11:36:11 +0200

    Links on front page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
+// import AstroDecapCms from "./src/astro-decap-cms/integration";
 import UnoCSS from "unocss/astro";
 
 export default defineConfig({
-  integrations: [UnoCSS({ injectReset: true })],
+  integrations: [
+    UnoCSS({ injectReset: true }),
+    // AstroDecapCms({ cmsConfig: { toto: "toto", titi: "tutu" } }),
+  ],
 });
