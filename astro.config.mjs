7215c4629d3b9a70a9529424da25a69b98f1ee88
Nicolas Rio <nicolas.rio42@gmail.com>
2024-05-02 09:40:21 +0200

    Main branch to CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import decapCms from "astro-decap";
 import { cmsConfig } from "./src/cms-config/config.js";
 
 export default defineConfig({
+  site: "https://lebouillondeseauxvives.fr/",
   integrations: [
     UnoCSS({
       injectReset: true,
